0fde01907ff386e82225c6843b742182d76faa6c
Eric Lee <erichanslee@gmail.com>
2022-03-28 10:11:43 -0700

    add re3 problems

--- metricdiversity/test_problems/problems.py
+++ metricdiversity/test_problems/problems.py
@@ -6,7 +6,8 @@ RE22_THRESHOLDS = [400, 2]  # About 10 percent
 RE23_THRESHOLDS = [2.5e5, -1]  # About 6 percent
 RE24_THRESHOLDS = [100, -7]  # About 8 percent
 RE25_THRESHOLDS = [20, 2] # About 6 percent
-
+RE31_THRESHOLDS = [80, 120, 150]  # About 4 percent
+RE33_THRESHOLDS = [2, 3, 4]  # About 4 percent
 
 def find_nearest_value(array, value):
   array = numpy.asarray(array)
@@ -285,6 +286,83 @@ class RE25(TestProblem):
     g[g < 0] = -g[g < 0]
     f2 = numpy.sum(g)
     return [f1, numpy.log10(f2 + 1e-6)]
+
+  def _evaluate(self, suggestion):
+    pt = numpy.array([suggestion[p] for p in self.parameter_names])
+    values = self.objective_functions(pt)
+    return values
+
+
+class RE31(TestProblem):
+  parameters = [
+    {'name': 'x0', 'type': 'double', 'bounds': {'min': 0.00001, 'max': 100}},
+    {'name': 'x1', 'type': 'double', 'bounds': {'min': 0.00001, 'max': 100}},
+    {'name': 'x2', 'type': 'double', 'bounds': {'min': 1, 'max': 3}},
+  ]
+
+  def __init__(self):
+    self.thresholds = RE31_THRESHOLDS
+    self.is_minimization = True
+
+  def objective_functions(self, x):
+    x1, x2, x3 = x
+
+    metric_0 = x1 * numpy.sqrt(16.0 + x3 ** 2) + x2 * numpy.sqrt(1.0 + x3 ** 2)
+
+    metric_1 = 20.0 * numpy.sqrt(16.0 + x3 ** 2)
+    metric_1 = metric_1 / (x3 * x1)
+
+    # Constraint functions
+    g = numpy.zeros(3)
+    g[0] = 0.1 - metric_0
+    g[1] = 100000.0 - metric_1
+    g[2] = 100000.0 - ((80.0 * numpy.sqrt(1.0 + x3 ** 2)) / (x3 * x2))
+
+    # Calculate the constratint violation values
+    g[g >= 0] = 0
+    g[g < 0] = -g[g < 0]
+    metric_2 = numpy.sum(g)
+
+    return [metric_0, metric_1, metric_2]
+
+  def _evaluate(self, suggestion):
+    pt = numpy.array([suggestion[p] for p in self.parameter_names])
+    values = self.objective_functions(pt)
+    return values
+
+
+class RE33(TestProblem):
+  parameters = [
+    {'name': 'x0', 'type': 'double', 'bounds': {'min': 55, 'max': 80}},
+    {'name': 'x1', 'type': 'double', 'bounds': {'min': 75, 'max': 110}},
+    {'name': 'x2', 'type': 'double', 'bounds': {'min': 1000, 'max': 3000}},
+    {'name': 'x3', 'type': 'double', 'bounds': {'min': 11, 'max': 20}},
+  ]
+
+  def __init__(self):
+    self.thresholds = RE33_THRESHOLDS
+    self.is_minimization = True
+
+  def objective_functions(self, x):
+    x1, x2, x3, x4 = x
+
+    metric_0 = 4.9 * 1e-5 * (x2 ** 2 - x1 ** 2) * (x4 - 1.0)
+
+    metric_1 = ((9.82 * 1e6) * (x2 ** 2 - x1 ** 2)) / (x3 * x4 * (x2**3 - x1**3))
+
+    # Constraint functions
+    g = numpy.zeros(4)
+    g[0] = (x2 - x1) - 20
+    g[1] = 0.4 - x3 / (3.14 * (x2 ** 2 - x1 ** 2))
+    g[2] = 1 - (2.22 * 1e-3 * x3 * (x2 ** 3 - x1 ** 3)) / ((x2 ** 2 - x1 ** 2) ** 2)
+    g[3] = 2.66 * 1e-2 * x3 * x4 * (x2 ** 3 - x1 ** 3) / (x2 ** 2 - x1 ** 2) - 900
+
+    # Calculate the constratint violation values
+    g[g >= 0] = 0
+    g[g < 0] = -g[g < 0]
+    metric_2 = numpy.sum(g)
+
+    return [metric_0, metric_1, metric_2]
 
   def _evaluate(self, suggestion):
     pt = numpy.array([suggestion[p] for p in self.parameter_names])
